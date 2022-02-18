869ab8962b208f98c85ca35693b8f2c154b5d127
Brett Seifried <seifriedbrett@gmail.com>
2022-02-18 15:07:34 -0800

    working group hover

--- src/Views/AboutMe.js
+++ src/Views/AboutMe.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function AboutMe() {
-  return <div className="aboutme">AboutMe</div>;
+  return (
+    <div className="aboutme">
+      <div>
+        <h1>About Me</h1>
+        <p>Father | Developer | Veteran</p>
+      </div>
+    </div>
+  );
 }
