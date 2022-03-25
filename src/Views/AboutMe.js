b4504aaec8104190e8132b5d3fd6dc9847ecfc06
Brett Seifried <seifriedbrett@gmail.com>
2022-03-24 17:39:59 -0700

    made transition for aboutme work

--- src/Views/AboutMe.js
+++ src/Views/AboutMe.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function AboutMe() {
+  const [aboutme, setStyle] = useState('aboutme');
+  const ToggleClass = () => {
+    setStyle(!aboutme);
+  };
+
   return (
-    <div className="aboutme">
-      <div>
+    <div>
+      <div onClick={ToggleClass} className={aboutme ? 'aboutme' : 'aboutmeBig'}>
         <h1>About Me</h1>
         <p>Father | Developer | Veteran</p>
       </div>
