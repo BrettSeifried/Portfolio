1fb5b836070c21fe3cc60a1d792916ccb13a78a7
Brett Seifried <seifriedbrett@gmail.com>
2022-03-25 17:31:55 -0700

    techSwap finished

--- src/Views/Project3.js
+++ src/Views/Project3.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
+import ProjectThreeDetails from '../Components/projectThree/ProjectThreeDetails';
+import shopping from '../assets/shopping.png';
 
 export default function Project3() {
+  const [project3, setStyle] = useState('project3');
+  const [hidden, setHidden] = useState('hidden');
+  const ToggleClass = () => {
+    setStyle(!project3);
+    setHidden(!hidden);
+  };
+
   return (
-    <div className="project3">
-      <div>
-        <h1>Project2 Title</h1>
-        <p>Short Descrip</p>
+    <div onClick={ToggleClass} className={project3 ? 'project3' : 'project3Big'}>
+      <h1>Alchemy Tech Swap</h1>
+      <p>React | Supabase | 4-Person team | 4 day-Sprint</p>
+      <img className="shopping" src={shopping} />
+      <div className={hidden ? 'hidden' : 'project3Show'}>
+        <ProjectThreeDetails />
       </div>
     </div>
   );
