ca599cf0c0de91ec3280b004f5c00e9104c1a814
Brett Seifried <seifriedbrett@gmail.com>
2022-04-01 15:53:29 -0700

    contact section good enough

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
+import computer from '../assets/computer.png';
+import ContactDetails from '../Components/contact/ContactDetails';
 
 export default function Contact() {
+  const [contact, setStyle] = useState('contact');
+  const [hidden, setHidden] = useState('hidden');
+  const ToggleClass = () => {
+    setStyle(!contact);
+    setHidden(!hidden);
+  };
+
   return (
-    <div className="contact">
-      <div>
-        <h1>Contact</h1>
+    <div onClick={ToggleClass} className={contact ? 'contact' : 'contactBig'}>
+      <h1 className="padding">Contact</h1>
+      <p className="padding">Email | LinkedIn</p>
+      <img className="computerSmall" src={computer} />
+      <div className={hidden ? 'hidden' : 'contactShow'}>
+        <ContactDetails />
       </div>
     </div>
   );
